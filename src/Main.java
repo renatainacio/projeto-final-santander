9b19d02c6891d0f0c317d2557f0a50db92be6dd0
renata <revazgauska@gmail.com>
2023-02-14 00:03:45 -0300

    fix: teste main

--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,7 @@ public class Main {
             System.out.println(cb.getTotalGolsPorRodada());
             System.out.println();
             System.out.println("Tabela:");
-            for(int i = 1; i < cb.getTodosOsTimes().size(); i++)
+            for(int i = 1; i <= cb.getTodosOsTimes().size(); i++)
             System.out.println(i + ". " + cb.getTabelaOrdenada().get(i - 1));
         }
         catch (IOException e){
